ceabb583c6828e93f55e4aacf130fdc41dfb0076
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-04-27 21:30:06 +0530

    set up file added

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+
+import AboutUs from "./Component/AboutUs";
+import Contact from "./Component/Contact";
+import Footer from "./Component/Footer";
+import Header from "./Component/Header";
+import Home from "./Component/Home";
+import SinglePost from "./Component/SinglePost";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home/>
+      <AboutUs/>
+      <Contact/>
+      <Footer />
+      <SinglePost/>
+    </>
+  );
+}
+
+export default App;
