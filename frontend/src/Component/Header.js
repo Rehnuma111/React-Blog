f1f0b09366322b87f5e48e708ca5158d9a565282
Rehnuma Bano <70592871+Rehnuma111@users.noreply.github.com>
2023-04-28 21:39:32 +0530

    adding routing

--- frontend/src/Component/Header.js
+++ frontend/src/Component/Header.js
@@ -1,111 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      {/* ======= Header ======= */}
       <header
         id="header"
         className="header d-flex align-items-center fixed-top"
       >
         <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
-          <a href="index.html" className="logo d-flex align-items-center">
-            {/* Uncomment the line below if you also wish to use an image logo */}
-            {/* <img src="assets/img/logo.png" alt=""> */}
+          <Link to="/" className="logo d-flex align-items-center">
             <h1>ZenBlog</h1>
-          </a>
+          </Link>
           <nav id="navbar" className="navbar">
             <ul>
               <li>
-                <a href="index.html">Blog</a>
+                <Link to="/">Blog</Link>
               </li>
               <li>
-                <a href="single-post.html">Single Post</a>
+                <Link to="/singlePost">Single Post</Link>
               </li>
-              <li className="dropdown">
-                <a href="category.html">
-                  <span>Categories</span>{" "}
-                  <i className="bi bi-chevron-down dropdown-indicator" />
-                </a>
-                <ul>
-                  <li>
-                    <a href="search-result.html">Search Result</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 1</a>
-                  </li>
-                  <li className="dropdown">
-                    <a href="#">
-                      <span>Deep Drop Down</span>{" "}
-                      <i className="bi bi-chevron-down dropdown-indicator" />
-                    </a>
-                    <ul>
-                      <li>
-                        <a href="#">Deep Drop Down 1</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 2</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 3</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 4</a>
-                      </li>
-                      <li>
-                        <a href="#">Deep Drop Down 5</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 2</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 3</a>
-                  </li>
-                  <li>
-                    <a href="#">Drop Down 4</a>
-                  </li>
-                </ul>
+
+              <li>
+                <Link to="/about">AboutUs</Link>
               </li>
               <li>
-                <a href="about.html">About</a>
+                <Link to="/contact">Contact</Link>
               </li>
               <li>
-                <a href="contact.html">Contact</a>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
               </li>
             </ul>
           </nav>
-          {/* .navbar */}
-          <div className="position-relative">
-            <a href="#" className="mx-2">
-              <span className="bi-facebook" />
-            </a>
-            <a href="#" className="mx-2">
-              <span className="bi-twitter" />
-            </a>
-            <a href="#" className="mx-2">
-              <span className="bi-instagram" />
-            </a>
-            <a href="#" className="mx-2 js-search-open">
-              <span className="bi-search" />
-            </a>
-            <i className="bi bi-list mobile-nav-toggle" />
-            {/* ======= Search Form ======= */}
-            <div className="search-form-wrap js-search-form-wrap">
-              <form action="search-result.html" className="search-form">
-                <span className="icon bi-search" />
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="form-control"
-                />
-                <button className="btn js-search-close">
-                  <span className="bi-x" />
-                </button>
-              </form>
-            </div>
-            {/* End Search Form */}
+
+          <div className="search-form-wrap js-search-form-wrap">
+            <form action="search-result.html" className="search-form">
+              <span className="icon bi-search" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="form-control"
+              />
+              <button className="btn js-search-close">
+                <span className="bi-x" />
+              </button>
+            </form>
           </div>
         </div>
       </header>
